01488fe9bf0fd55e01f2150dd8c5e67331751b6f
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-02-03 21:29:51 -0600

    Generated Vite project

---
+++ personal-portfolio-v2/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>AV</p>
+            <div>
+                <p>Home</p>
+                <p>Projects</p>
+                <p>Work</p>
+                <p>Contact</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
