58426f27bb5f48143aa3eb40a4c2e8b94b4d2508
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-02-29 20:12:51 -0600

    Updated styling for nav bar and changed a few other bits of styling.

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="bg-[#131313]">
-      <nav className="flex w-screen items-center justify-between bg-[#131313] text-white border border-red-400">
+      <nav className="flex w-[100%] items-center justify-between bg-[#131313] p-[5px] text-white border border-red-400">
         <h1>AV</h1>
         <div className="flex justify-around items-center w-[400px] border border-red-400">
           <p>Home</p>
@@ -16,13 +16,13 @@ const App = () => {
           <p>Contact</p>
         </div>
         <div className="flex w-[100px] align-items justify-around border border-red-400">
-          <Icon icon="pajamas:twitter" />
-          <Icon icon="mdi:instagram" />
-          <Icon icon="mdi:github" />
-          <Icon icon="mdi:linkedin" />
+          <a href="https://twitter.com/av_iex" target="_blank"><Icon icon="pajamas:twitter" /></a>
+          <a href="https://www.instagram.com/av.iex/" target="_blank"><Icon icon="mdi:instagram" /></a>
+          <a href="https://github.com/alexandroivaldez" target="_blank"><Icon icon="mdi:github" /></a>
+          <a href="https://www.linkedin.com/in/alexandro-valdez/" target="_blank"><Icon icon="mdi:linkedin" /></a>
         </div>
       </nav>
-      <div className="flex bg-[#131313] text-white w-screen justify-center border border-green-500 mt-[100px]">
+      <div className="flex bg-[#131313] text-white w-[100%] justify-center border border-green-500 mt-[100px]">
         <div className="flex flex-col justify-center border border-red-500">
           <h1 className="text-[30px]">
             Hi, I&apos;m <span>{`<AlexandroValdez />`}</span>
@@ -36,15 +36,15 @@ const App = () => {
         </div>
         <img src={chibiAlex} alt="Art photo of a developer"  className=" w-[40%] border-red-400"/>
       </div>
-      <div className="flex flex-col w-screen justify-center items-center m-auto bg-[#131313] text-white border border-red-500">
+      <div className="flex flex-col w-[100%] justify-center items-center m-auto bg-[#131313] text-white border border-red-500">
         <p>My Projects</p>
         <Icon icon="material-symbols:arrow-drop-down" />
       </div>
-      <div className="flex flex-col w-screen border border-purple-500">
-        <div className="flex border border-orange-500 w-[1000px] justify-center ml-auto mr-auto">
+      <div className="flex flex-col w-[100%] border border-purple-500 gap-[15px]">
+        <div className="flex border border-orange-500 w-[100%] justify-center ml-auto mr-auto">
           <img src={websiteImg} alt="Picture of a website." width={"400px"} />
-          <div className="flex flex-col">
-            <h1>{`<DevLog />`}</h1>
+          <div className="flex flex-col text-white">
+            <h1 className="text-[40px]">{`<DevLog />`}</h1>
             <p>
               I built this website to keep track of most of my development
               tasks. It was built using React, TypeScript, Tailwind, MongoDB,
@@ -53,9 +53,9 @@ const App = () => {
             <button>Live Demo</button>
           </div>
         </div>
-        <div className="flex border border-orange-500 w-[1000px] justify-center ml-auto mr-auto">
-          <div className="flex flex-col">
-            <h1>{`<DevLog />`}</h1>
+        <div className="flex border border-orange-500 w-[100%] justify-center ml-auto mr-auto">
+          <div className="flex flex-col text-white">
+            <h1 className="text-[40px]">{`<DevLog />`}</h1>
             <p>
               I built this website to keep track of most of my development
               tasks. It was built using React, TypeScript, Tailwind, MongoDB,
